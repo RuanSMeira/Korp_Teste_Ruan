bfe38a74ce9319bd71dc0eae04c83b7ceea17537
Ruan Silva Meira <ruansilvameira@gmail.com>
2026-08-30 23:20:12 -0300

    first commit

---
+++ src/Korp_Teste_Ruan.Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Korp_Teste_Ruan.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
